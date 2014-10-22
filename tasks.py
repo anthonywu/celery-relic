0a0ac9fff817800b084aa41824794c3d1ae12eed
Anthony Wu <wu@learnsprout.com>
2014-10-22 15:00:39 -0700

    clean up debug traceback

--- tasks.py
+++ tasks.py
@@ -9,13 +9,13 @@ my_app = Celery('tasks', backend='amqp', broker=os.environ['CELERY_RELIC_BROKER_
 @my_app.task
 def add(x, y):
     """The usual hello world demo."""
-    traceback.print_stack()
+    # traceback.print_stack()
     return x + y
 
 @my_app.task
 def add_long_time(x, y):
     """Take a long time to add two numbers in order to mock expensive transactions."""
-    traceback.print_stack()
+    # traceback.print_stack()
     wait = random.randint(1, 5)
     print("Will wait {} seconds".format(wait))
     time.sleep(wait)
