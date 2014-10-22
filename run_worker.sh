eb5b54d749a6d853f9956165c35c2cf705f42e54
Anthony Wu <wu@learnsprout.com>
2014-10-21 18:05:10 -0700

    turn on newrelic monitoring

--- run_worker.sh
+++ run_worker.sh
@@ -9,6 +9,11 @@ export CELERY_TRACE_APP=1
 
 if [[ -f $NEW_RELIC_CONFIG_FILE ]]; then
   celery_worker_args="--app=tasks.my_app --concurrency=1 --loglevel=debug"
-  celery worker $celery_worker_args
-# newrelic-admin run-program celery -A tasks worker --loglevel=debug
+  if [[ -n "$NEW_RELIC_LICENSE_KEY" ]]; then
+    echo "INFO: running with NewRelic monitoring"
+    newrelic-admin run-program celery worker $celery_worker_args
+  else
+    echo "WARN: running without NewRelic monitoring" >&2
+    celery worker $celery_worker_args
+  fi
 fi
