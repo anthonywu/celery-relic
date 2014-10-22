688f33c7c40da489c52c8500ddc654b2bc933e38
Anthony Wu <wu@learnsprout.com>
2014-10-21 17:45:15 -0700

    import celery for shell

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env ipython -i
 
+import celery
+
 from tasks import add, add_long_time
 
 print(add.name)
@@ -8,5 +10,3 @@ print(add_long_time.name)
 def do():
     r1 = add.delay(1, 2)
     return r1.ready(), r1
-
-
