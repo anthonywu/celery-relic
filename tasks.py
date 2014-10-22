4584259d91d057f2deb37d32dc06f1558ad4fc3b
Anthony Wu <wu@learnsprout.com>
2014-10-22 10:53:31 -0700

    NR support advises debug traceback

--- tasks.py
+++ tasks.py
@@ -2,17 +2,20 @@ from celery import Celery
 import os
 import random
 import time
+import traceback
 
 my_app = Celery('tasks', backend='amqp', broker=os.environ['CELERY_RELIC_BROKER_URL'])
 
 @my_app.task
 def add(x, y):
     """The usual hello world demo."""
+    traceback.print_stack()
     return x + y
 
 @my_app.task
 def add_long_time(x, y):
     """Take a long time to add two numbers in order to mock expensive transactions."""
+    traceback.print_stack()
     wait = random.randint(1, 5)
     print("Will wait {} seconds".format(wait))
     time.sleep(wait)
