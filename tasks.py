751a438c0a23b1518d4b0242c0ebd68dbc6f5db2
Anthony Wu <wu@learnsprout.com>
2014-10-21 18:08:15 -0700

    doc fix

--- tasks.py
+++ tasks.py
@@ -12,7 +12,7 @@ def add(x, y):
 
 @my_app.task
 def add_long_time(x, y):
-    "Take a long time to add two numbers in order to mock expensive transactions."
+    """Take a long time to add two numbers in order to mock expensive transactions."""
     wait = random.randint(1, 5)
     print("Will wait {} seconds".format(wait))
     time.sleep(wait)
